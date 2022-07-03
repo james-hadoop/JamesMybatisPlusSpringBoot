2c56b68606ea182911eaba38fb9b325bccd338db
James John <james_hadoop@163.com>
2022-07-04 00:11:33 +0800

    update

--- james-mpsb-auth/src/main/java/com/james/mpsb/auth/config/MybatisPlusConfig.java
+++ james-mpsb-auth/src/main/java/com/james/mpsb/auth/config/MybatisPlusConfig.java
@@ -10,24 +10,24 @@ import org.springframework.transaction.annotation.EnableTransactionManagement;
  * @author jl
  * @since 2019-11-27 15:44
  */
-@EnableTransactionManagement
-@Configuration
+//@EnableTransactionManagement
+//@Configuration
 public class MybatisPlusConfig {
-
-    /**
-     * 乐观锁插件
-     */
-    @Bean
-    public OptimisticLockerInterceptor optimisticLockerInterceptor() {
-        return new OptimisticLockerInterceptor();
-    }
-
-    /**
-     * 分页插件
-     */
-    @Bean
-    public PaginationInterceptor paginationInterceptor() {
-        return new PaginationInterceptor();
-    }
+//
+//    /**
+//     * 乐观锁插件
+//     */
+//    @Bean
+//    public OptimisticLockerInterceptor optimisticLockerInterceptor() {
+//        return new OptimisticLockerInterceptor();
+//    }
+//
+//    /**
+//     * 分页插件
+//     */
+//    @Bean
+//    public PaginationInterceptor paginationInterceptor() {
+//        return new PaginationInterceptor();
+//    }
 
 }
